c7257c935e5830e4a865f8f548097caa5c4d1e7b
Mr.Lin <Mr.Lin@DESKTOP-OGDGSHL>
2019-06-29 16:41:38 +0800

    绝望的提交

---
+++ src/test/java/com/gdkm/weixin/Weixin3ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gdkm.weixin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Weixin3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
